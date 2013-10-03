4d02da3f776679de222e0c76f481d01f18d6cb18
Apprentice <apprentice@dbc26.local>
2013-10-02 21:45:26 -0700

    changes to student.rb and 20121011144238_create_students.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,19 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+  validates :email, :uniqueness => true
+  validates :email, :format => { :with => /\w+@\w+.\w{2,}/}
+  validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+  :phone.to_s.gsub!(/\D/, '')
+  validates :phone, :format => { :with => /(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?/ }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    Time.now.year - birthday.year
+  end
 end
+
+
